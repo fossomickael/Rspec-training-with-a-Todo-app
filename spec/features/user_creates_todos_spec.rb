c80d3fe3d7050027a3f23e3ee32dbab75dd74495
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-18 16:12:05 +0200

    allow user to create todos

---
+++ spec/features/user_creates_todos_spec.rb
@@ -0,0 +1,14 @@
+require 'rails_helper'
+
+
+feature 'user create todo' do
+  scenario 'successfully' do
+    visit root_path
+
+    click_on "Add a new todo"
+    fill_in "Title" , with: "Buy Milk"
+    click_on "Submit"
+
+    expect(page).to have_css '.todos li', text: 'Buy Milk'
+  end
+end
