af169e7870f136f73d08d7a79f78cb97f00c6472
Mickael FOSSO <fosso.mickael@gmail.com>
2021-06-20 09:52:43 +0200

    completed_at refactoring

--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,14 +1,29 @@
 require 'rails_helper'
 
-describe Todo, "#completed?" do
-  it "returns true if completed_at is set" do
+describe Todo, '#completed?' do
+  it 'returns true if completed_at is set' do
     todo = Todo.new(completed_at: Time.current)
     expect(todo).to be_completed
   end
 
-  it "returns false if completed_at is nill" do
+  it 'returns false if completed_at is nill' do
     todo = Todo.new(completed_at: nil)
     expect(todo).not_to be_completed
+  end
+
+  describe Todo, "#complete!" do
+    it "update completed_at" do
+      somebody = User.create!(email: 'somebody@example.com')
+      todo = Todo.new(completed_at: nil)
+      todo.user = somebody
+      
+      todo.save!
+      
+      todo.complete!
+      
+      todo.reload
+      expect(todo).to be_completed
+    end
   end
 
 end
